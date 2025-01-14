30556f337ea1127fab214f472c438cffa713555e
neo-y-u <horo2424@gmail.com>
2025-01-14 18:00:52 +0900

    Update Google_Drive_Layout_Toggle.user.js
    test

--- Google_Drive_Layout_Toggle.user.js
+++ Google_Drive_Layout_Toggle.user.js
@@ -2,7 +2,7 @@
 // @name         Google Drive Layout Toggle
 // @namespace    https://github.com/neo-y-u/Userscript
 // @updateURL    https://github.com/neo-y-u/Userscript/raw/refs/heads/main/Google_Drive_Layout_Toggle.user.js
-// @version      1.3.1
+// @version      1.3.2
 // @description  Toggle Google Drive's list/grid layout using the "Q" key.
 // @author       neo-y-u
 // @match        https://drive.google.com/*
