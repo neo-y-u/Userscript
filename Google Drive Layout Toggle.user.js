44fd70832acd963577fb78366ca6c16fca184093
neo-y-u <horo2424@gmail.com>
2025-01-14 17:48:15 +0900

    Update Google Drive Layout Toggle.user.js
    push Z→V

--- Google Drive Layout Toggle.user.js
+++ Google Drive Layout Toggle.user.js
@@ -2,8 +2,8 @@
 // @name         Google Drive Layout Toggle
 // @namespace    https://github.com/neo-y-u/Userscript
 // @updateURL    https://github.com/neo-y-u/Userscript/Google Drive Layout Toggle.user.js
-// @version      1.2
-// @description  Toggle Google Drive's list/grid layout using the "Z" key.
+// @version      1.3
+// @description  Toggle Google Drive's list/grid layout using the "V" key.
 // @author       neo-y-u
 // @match        https://drive.google.com/*
 // @grant        none
@@ -15,7 +15,7 @@
     // Listen for the "-" key press
     document.addEventListener('keydown', function (event) {
         // Check if the pressed key is "-" and no modifier keys are active
-        if (event.key === 'z' && !event.ctrlKey && !event.altKey && !event.metaKey) {
+        if (event.key === 'v' && !event.ctrlKey && !event.altKey && !event.metaKey) {
             // Prevent default behavior
             event.preventDefault();
 
