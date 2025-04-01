92fb102b3cdf80fcd9f134790b8ab7d99da51c8b
neo-y-u <horo2424@gmail.com>
2025-04-02 08:56:01 +0900

    Update GoogleDocs_PolicyBanner_Remover.user.js
    Enable at Google Drive

--- GoogleDocs_PolicyBanner_Remover.user.js
+++ GoogleDocs_PolicyBanner_Remover.user.js
@@ -5,6 +5,7 @@
 // @description  「ポリシーに準拠するには～」バナーを完全に非表示にする
 // @author       neo-y-u
 // @icon         https://w7.pngwing.com/pngs/990/967/png-transparent-google-file-application-google-docs-document-google-sheets-google-drive-google-plus-angle-rectangle-logo.png
+// @match        https://drive.google.com/*
 // @match        https://docs.google.com/document/*
 // @match        https://docs.google.com/presentation/*
 // @match        https://docs.google.com/spreadsheets/*
